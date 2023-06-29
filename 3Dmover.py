b6583e17369a0345b433e77331b2f63e48e4d91d
noellymadeleine <noelly.madeleine@gmail.com>
2023-06-29 22:34:58 +0200

    Correct Docsstrings of the functions

--- 3Dmover.py
+++ 3Dmover.py
@@ -41,6 +41,15 @@ import argparse
 
 
 def parse_arguments():
+    """
+    Parse the command-line arguments for 3Dmover tool.
+
+    Returns:
+        argparse.Namespace: An object containing the parsed command-line arguments.
+
+    Raises:
+        argparse.ArgumentError: If the command-line arguments are invalid.
+    """
     parser = argparse.ArgumentParser(description='3Dmover: A Tool for Manipulating 3D Protein Objects')
 
     parser.add_argument('pdb_file', type=str, help='Path to the PDB file')
@@ -54,38 +63,20 @@ def parse_arguments():
 
 def move_pdb(pdb_file, center=True, translation=None):
     """
-    3Dmover: A Tool for Manipulating 3D Protein Objects
+    Read a PDB file and modify the atom coordinates by centering and/or translating them.
 
-    3Dmover is a versatile tool designed to manipulate 3D protein objects in space. It provides functionality to
-    center the protein at the origin (0, 0, 0) or perform translations to move the protein in 3D space.
+    Args:
+        pdb_file (str): The path to the PDB file.
+        center (bool, optional): Flag indicating whether to center the atom coordinates. Defaults to True.
+        translation (tuple or None, optional): The translation vector as a tuple of three floats (x, y, z).
+            If provided, the atom coordinates will be translated by adding the translation vector to each coordinate.
+            Defaults to None.
 
-    Usage: python 3Dmover.py <pdb_file> [-c] [-t X Y Z]
+    Returns:
+        str: The path of the updated PDB file with modified coordinates.
 
-    The tool reads a PDB file containing the atom coordinates of a protein object, applies the desired manipulation
-    (transformation), and saves the updated coordinates in a new PDB file.
-
-    Arguments:
-        pdb_file         Path to the PDB file
-
-    Options:
-        -c, --center     Center the protein at the origin (0, 0, 0)
-        -t, --translate  Translate the protein by the specified amounts in X, Y, and Z directions
-
-    Example:
-        python 3Dmover.py protein.pdb -c
-
-        This will center the protein object in the specified PDB file at the origin (0, 0, 0) and save the updated
-        coordinates in a new PDB file with the suffix '_centered' appended to the original filename.
-
-        python 3Dmover.py protein.pdb -t 1.0 2.0 3.0
-
-        This will translate the protein object in the specified PDB file by the amounts (1.0, 2.0, 3.0) in the X, Y,
-        and Z directions, and save the updated coordinates in a new PDB file with the suffix '_translated' appended
-        to the original filename.
-
-    Note: The PDB file must follow the standard format for atomic coordinate data.
-
-    For more information and options, refer to the documentation.
+    Raises:
+        FileNotFoundError: If the specified PDB file does not exist.
     """
 
     # Read the PDB file and extract the atom coordinates
